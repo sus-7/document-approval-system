b718a9286aa8ef6da5cc580678fd07aa8b37df06
Neeraj! <neerajsurnis143r@gmail.com>
2025-01-08 22:42:20 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ForgotPassword from "./components/ForgotPassword";
 import RemarkUI from "./components/RemarkUI";
 import Notifications from "./components/Notifications";
 import ManageUsers from "./components/ManageUsers";
+import CM_New from "./components/CM_New";
 const App = () => {
   return (
    
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/remark-pdf" element={<RemarkUI />} />
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/users/manage" element={<ManageUsers />} />
+        <Route path="cm-new" element={<CM_New/>} />
         
       </Routes>
     
