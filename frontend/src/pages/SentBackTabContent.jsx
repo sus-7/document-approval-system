d68dbe74173b457a9772e04ac92a009275ec259e
Neeraj! <neerajsurnis143r@gmail.com>
2025-02-09 10:53:19 +0530

    s

--- frontend/src/pages/SentBackTabContent.jsx
+++ frontend/src/pages/SentBackTabContent.jsx
@@ -64,7 +64,7 @@ const SentBackTabContent = ({
       }
 
       const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/file/get-documents?status=correction`,
+        `${import.meta.env.VITE_API_URL}/file/get-documents?status=correction-rejected`,
         { withCredentials: true }
       );
 
