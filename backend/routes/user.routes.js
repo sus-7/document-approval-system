a548aecbc36b7d9e96344807f0207faae6e1d2b9
Shreyash Galgale <shreyasgalgale019@gmail.com>
2025-02-15 22:43:56 +0530

    cleanup

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,29 +1,15 @@
 const express = require("express");
 const {
-    signIn,
-    signUp,
-    signOut,
     signOutAll,
-    // verifyOTP,
-    resendOTPAndVerify,
-    checkAuthStatus,
-    sendPasswordResetOTP,
     resetPassword,
-    verifySpOTP,
     updateProfile,
-    getAssistants,
     //v2
     sendCredentials,
     setUserStatus,
     getUsers,
 } = require("../controllers/user.controllers");
 const {
-    signUpDetailsValidator,
-    signiInDetailsValidator,
     verifyToken,
-    verifySpToken,
-    verifyEmailExists,
-    verifyOldPassword,
     resetPasswordValidator,
     //v2
     updateProfileValidator,
@@ -38,39 +24,7 @@ const {
 const { Role } = require("../utils/enums");
 const router = express.Router();
 
-// router.post("/signup", signUpDetailsValidator, signUp);
-// router.post("/signin", signiInDetailsValidator, signIn);
-// router.post("/signout", verifyToken, signOut);
 router.post("/signout-all", verifyToken, signOutAll);
-// router.get("/status", verifyToken, checkAuthStatus);
-
-// router.post("/verify-otp", verifyOTP);
-// router.post("/resend-otp", resendOTPAndVerify);
-
-// router.post(
-//     "/send-password-reset-otp",
-//     verifyEmailExists,
-//     sendPasswordResetOTP
-// );
-// router.post("/verify-sp-otp", verifySpOTP);
-// router.post("/reset-password", verifySpToken, resetPassword);
-// router.post("/change-password", verifyToken, verifyOldPassword, resetPassword);
-// router.post("/update-profile", verifyToken, updateProfile);
-
-//route for toggling user status
-// router.post(
-//     "/change-user-status",
-//     verifyToken,
-//     authorizeRoles([Role.SENIOR_ASSISTANT, Role.ADMIN]),
-//     changeUserStatus
-// );
-
-// router.get(
-//     "/get-assistants",
-//     verifyToken,
-//     authorizeRoles([Role.APPROVER]),
-//     getAssistants
-// );
 
 //v2
 router.post(
